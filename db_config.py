6de0b312e86989f442aafa8eaf3aa1d37f682fd3
itzrao <itzrao@gmail.com>
2025-05-16 20:35:50 +0530

    after aws

--- db_config.py
+++ db_config.py
@@ -1,9 +1,26 @@
 import mysql.connector
+import boto3
+import json
+
+def get_db_secret():
+    secret_name = "prod/rds/mysql"
+    region_name = "us-west-2"  # e.g., "ap-south-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    response = client.get_secret_value(SecretId=secret_name)
+    secret = json.loads(response['SecretString'])
+    return secret
 
 def get_connection():
+    secret = get_db_secret()
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",  # Add your MySQL password here
-        database="student_leave_system"
+        host=secret['host'],
+        user=secret['username'],
+        password=secret['password'],
+        database=secret['dbname']
     )
