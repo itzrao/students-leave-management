86db9ad96c41f00e8ece95336f7b5d9556aa3236
ADDAGIRI SUBBARAO <65004463+itzrao@users.noreply.github.com>
2025-05-27 09:11:08 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="db-nielit.cl6muqgg8bt3.ap-south-1.rds.amazonaws.com",
-        user="admin",
-        password="Sai909868",  # Add your MySQL password here
+        host="xxxxx.ap-south-1.rds.amazonaws.com",
+        user="xxxx",
+        password="xxxxx",  # Add your MySQL password here
         database="leave_system"
     )
